aac4dabe3dc8048a341cc413622f39e0faf36a7a
Sameer <sameerdutta9703@gmail.com>
2020-01-28 13:37:04 +0530

    task 6

---
+++ Task 8/IDisposeApplication/GarbageCollector/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDisposeApplication;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        static void MakeSomeGarbage()
+        {
+            Object obj;
+            for (int i = 0; i < 100000; i++)
+            {
+                obj = new Object();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Making some garbage
+            Program.MakeSomeGarbage();
+            Console.WriteLine("Memory used before collection : {0}", GC.GetTotalMemory(false));
+
+            //Collecting the garbage
+            GC.Collect();
+            Console.WriteLine("Memory used after full collection : {0}", GC.GetTotalMemory(true));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task 8/IDisposeApplication/IDisposeApplication/IDisposeImplementation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposeApplication
+{
+    class IDisposeImplementation : IDisposable
+    {
+        bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+
+            //To show that we are getting the functionality of the Dipose methord
+            Console.WriteLine("In the Dispose Methord");
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                Console.WriteLine("Cleaned all the Managed Code");
+                //free the managed objects here;
+            }
+
+            Console.WriteLine("Cleaned all the Unmanaged Code");
+            //free the unmanaged objects
+
+            disposed = true;
+            
+            Console.ReadLine();
+        }
+
+        // The Deconstructor
+        ~IDisposeImplementation()
+        {
+            Dispose(false);
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            using (IDisposeImplementation obj = new IDisposeImplementation())
+            {                
+                //obj.Dispose();
+            }
+           
+        }
+    }
+}
