953eec85c96458bd0c98a7e226d23e6d6bff40c7
Sameer <sameerdutta9703@gmail.com>
2020-01-30 15:36:26 +0530

    Task 10

---
+++ Task 10/DeligatesDemo/ComparisionDeligate/ComparisionDeligateClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisionDeligate
+{
+    class ComparisionDeligateClass
+    {
+        //Declaration of the variables
+        string stuName;
+        string stuSchool;
+        int stuRoll;
+
+        //The constructor for initialization
+        public ComparisionDeligateClass(string name, string school, int roll)
+        {
+            this.stuName = name;
+            this.stuSchool = school;
+            this.stuRoll = roll;
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                return this.stuName;
+            }
+        }
+        public string School
+        {
+            get
+            {
+                return this.stuSchool;
+            }
+        }
+        public int Roll
+        {
+            get
+            {
+                return this.stuRoll;
+            }
+        }
+
+        //Just for the name 
+        public static int compareByName(ComparisionDeligateClass obj1, ComparisionDeligateClass obj2)
+        {
+            return String.Compare(obj1.Name, obj2.Name);
+        }
+
+        //For the combination of name and school
+        public static int compareByNameAndSchool(ComparisionDeligateClass obj1, ComparisionDeligateClass obj2)
+        {
+            return String.Compare(obj1.School + obj1.Name, obj2.School + obj2.Name);
+        }
+
+        //For the roll number
+        public static int compareByRoll(ComparisionDeligateClass obj1, ComparisionDeligateClass obj2)
+        {
+            return obj1.Roll.CompareTo(obj2.Roll);
+        }
+
+
+        //The Display Function for printing the array
+        private static void DisplayArray(ComparisionDeligateClass[] student)
+        {
+            Console.WriteLine("{0,-20} {1,-25} {2,10}", "Name", "School", "Roll");
+            foreach (var i in student)
+                Console.WriteLine("{0,-20} {1,-25} {2,10:N0}", i.Name,
+                                  i.School, i.Roll);
+            Console.WriteLine("\n");
+        }
+
+        //The Main Function for this Class
+        static void Main(string[] args)
+        {
+            ComparisionDeligateClass Sam = new ComparisionDeligateClass("Sameer", "NHES", 23);
+            ComparisionDeligateClass Apu = new ComparisionDeligateClass("Apurv", "Loyala", 68);
+            ComparisionDeligateClass Jay = new ComparisionDeligateClass("Jayaa", "Tagore", 18);
+
+            ComparisionDeligateClass[] students = { Sam, Apu, Jay };
+
+            //Displaying the ArrayList
+            DisplayArray(students);
+
+            //Sorting by the student name
+            Array.Sort(students, ComparisionDeligateClass.compareByName);
+            DisplayArray(students);
+
+            //Sorting by the roll number
+            Array.Sort(students, ComparisionDeligateClass.compareByRoll);
+            DisplayArray(students);
+
+            //Sorting the array by name + school
+            Array.Sort(students, ComparisionDeligateClass.compareByNameAndSchool);
+            DisplayArray(students);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task 10/DeligatesDemo/DeligatesDemo/DeligateDemoClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeligatesDemo
+{
+    class DeligateDemoClass
+    {
+        //Declaring the deligates
+        public delegate void numAddition(int a, int b);
+        public delegate void numSubtraction(int a, int b);
+
+        //The  Addition methord
+        public void sum(int a, int b)
+        {
+            Console.WriteLine("(100 + 40) = {0}", a + b);
+        }
+
+        //The Subtraction methord
+        public void diff(int a, int b)
+        {
+            Console.WriteLine("(100 - 40) = {0}", a - b);
+        }
+
+        static void Main(string[] args)
+        {
+
+            //Creating the object of the DeligateDemoClass
+            DeligateDemoClass obj = new DeligateDemoClass();
+
+            //Creating the object of Deligates for the sum and diff methord
+            numAddition addObj = new numAddition(obj.sum);
+            numSubtraction diffObj = new numSubtraction(obj.diff);
+
+            //Pass the values of the methord by the deligate object
+            addObj(100, 40);
+
+            //We can use the invoke methord too for calling the deligate objects
+            diffObj.Invoke(100, 40);
+
+            Console.ReadLine();
+        }
+    }
+}
