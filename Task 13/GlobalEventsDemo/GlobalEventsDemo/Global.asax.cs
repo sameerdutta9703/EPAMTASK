53128f9810c536a0b4a66ff04e71f8bf71b19c00
Sameer <sameerdutta9703@gmail.com>
2020-02-05 18:34:58 +0530

    task 13

---
+++ Task 13/GlobalEventsDemo/GlobalEventsDemo/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace GlobalEventsDemo
+{
+    public class Global : HttpApplication
+    {
+
+        void Application_Init(object sender, EventArgs e)
+        {
+        }
+
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        void Application_BeginRequest(object sender, EventArgs e)
+        {
+        }
+
+        void Application_EndRequest(Object sender, EventArgs e)
+        {
+        }
+
+        void Application_AuthenticateRequest(Object sender, EventArgs e)
+        {
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+        }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+        }
+    }
+}
