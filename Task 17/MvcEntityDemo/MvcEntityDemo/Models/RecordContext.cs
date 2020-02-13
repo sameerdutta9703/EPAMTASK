e8827d1ded384edf48d57da553036e0a3e19bf9b
Sameer <sameerdutta9703@gmail.com>
2020-02-13 17:41:19 +0530

    Task 17

---
+++ Task 17/MvcEntityDemo/MvcEntityDemo/Models/RecordContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcEntityDemo.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MvcEntityDemo.Models
+{
+    public class RecordContext : DbContext
+    {
+        public RecordContext() : base("RecordContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Task 17/MvcEntityDemo/MvcEntityDemo/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcEntityDemo.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Course { get; set; }
+        public string Contact { get; set; }
+    }
+}
