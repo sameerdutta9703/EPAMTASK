7516a3dc9870361e0a80df13776a29460494f43e
Sameer <sameerdutta9703@gmail.com>
2020-03-02 16:13:22 +0530

    changes

--- DEP Tasks/Trainers Task/JSCore/Task 2/Word Count.js
+++ DEP Tasks/Trainers Task/JSCore/Task 2/Word Count.js
@@ -1,5 +1,5 @@
-Questions
-http://www.codewars.com/kata/word-count
+// Questions
+// http://www.codewars.com/kata/word-count
 
 function countWords(str) {
     return str.trim().split(/\s/).filter(Boolean).length;
