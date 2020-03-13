5c36de4d5a845782c535a0c143f1e1e6482cec26
Sameer <sameerdutta9703@gmail.com>
2020-03-13 16:31:01 +0530

    used arrow functions

--- DEP Tasks/Trainers Task/JSCore/Task 5/Array-Helpers.js
+++ DEP Tasks/Trainers Task/JSCore/Task 5/Array-Helpers.js
@@ -2,30 +2,20 @@
 // https://www.codewars.com/kata/array-helpers
 
 Array.prototype.square = function () {
-  return this.map(function (n) {
-    return n * n;
-  });
+  return this.map(n => Math.pow(n, 2));
 }
 Array.prototype.cube = function () {
-  return this.map(function (n) {
-    return n * n * n;
-  });
+  return this.map(n => Math.pow(n, 3));
 }
 Array.prototype.sum = function () {
-  return this.reduce(function (a, b) {
-    return a + b;
-  }, 0);
+  return this.reduce((a, b) => a + b, 0);
 }
 Array.prototype.average = function () {
   return this.sum() / this.length;
 }
 Array.prototype.even = function () {
-  return this.filter(function (item) {
-    return 0 == item % 2;
-  });
+  return this.filter(n => (n % 2 === 0));
 }
 Array.prototype.odd = function () {
-  return this.filter(function (item) {
-    return 0 != item % 2;
-  });
+  return this.filter(n => (n % 2 !== 0));
 }
